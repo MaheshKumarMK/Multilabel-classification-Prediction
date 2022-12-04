62395eab67faf3ee75c59fa7e34cc77646a4adf8
MaheshKumarMK <maheshmkvb92@gmail.com>
2022-12-04 19:00:57 +0530

    Data validation process done

--- Ratings/entity/artifact_entity.py
+++ Ratings/entity/artifact_entity.py
@@ -5,3 +5,19 @@ class DataIngestionArtifact:
     trained_file_path: str
 
     test_file_path: str
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+
+    valid_train_file_path: str
+
+    valid_test_file_path: str
+
+    invalid_train_file_path: str
+
+    invalid_test_file_path: str
+
+    drift_report_file_path: str
+
+    std_dev_report_file_path: str
--- Ratings/pipeline/training_pipeline.py
+++ Ratings/pipeline/training_pipeline.py
@@ -4,10 +4,17 @@ from ratings.components.data_ingestion import DataIngestion
 from ratings.exception import RatingsException
 from ratings.logger import logging
 
-from ratings.entity.config_entity import TrainingPipelineConfig
-from ratings.entity.config_entity import DataIngestionConfig
-from ratings.entity.artifact_entity import DataIngestionArtifact
+from ratings.entity.config_entity import (
+    TrainingPipelineConfig, 
+    DataIngestionConfig,
+    DataValidationConfig
+)
+from ratings.entity.artifact_entity import (
+    DataIngestionArtifact,
+    DataValidationArtifact
+)
 from ratings.components.data_ingestion import DataIngestion
+from ratings.components.data_validation import DataValidation
 
 from ratings.constant.training_pipeline import *
 
@@ -17,6 +24,8 @@ class TrainPipeline:
         self.training_pipeline_config = TrainingPipelineConfig()
 
         self.data_ingestion_config=DataIngestionConfig()
+
+        self.data_validation_config= DataValidationConfig()
 
 
     def start_data_ingestion(self)->DataIngestionArtifact: #this function should return train and test file path as mentioned in artifact
@@ -44,13 +53,39 @@ class TrainPipeline:
         except Exception as e:
             raise RatingsException(e, sys)
 
+
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise RatingsException(e, sys) from e
+
     def run_pipeline(self):
 
-        TrainPipeline.is_pipeline_running=True
 
         try:
             data_ingestion_artifact = self.start_data_ingestion()
 
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+
         except Exception as e:
-            TrainPipeline.is_pipeline_running=False
+    
             raise RatingsException(e, sys)
