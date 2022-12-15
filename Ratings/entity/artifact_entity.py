35006859872c8af60ffe86bc365e8c2c16f7d003
MaheshKumarMK <maheshmkvb92@gmail.com>
2022-12-15 20:54:06 +0530

    data transformation process

--- Ratings/entity/artifact_entity.py
+++ Ratings/entity/artifact_entity.py
@@ -21,3 +21,12 @@ class DataValidationArtifact:
     drift_report_file_path: str
 
     std_dev_report_file_path: str
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_file_path: str
+
+    transformed_test_file_path: str
+
+    transformed_object_file_path: str
