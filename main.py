dd17dc0a48e24f44bf39a7a2f562ecdfc6a4c74f
MaheshKumarMK <maheshmkvb92@gmail.com>
2022-12-22 00:30:49 +0530

    updated main

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ async def train_route():
 #         df['predicted_column'] = y_pred
 #         df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
         
-#         #decide how to return file to user.
         
 #     except Exception as e:
 #         raise Response(f"Error Occured! {e}")
